5b18b9a9db069faa997d943549d27ab485bf724a
sepidehDarani <darandashisepideh99@gmail.com>
2026-09-04 22:24:30 +0430

    [add]: add file

---
+++ 1.py
@@ -0,0 +1,6 @@
+a=int (input("عدد اول را وارد کنید"))
+b=int(input("عدد دوم را وارد کنید"))
+
+while a<b:
+    print(a)
+    a+=1
---
+++ test.py
@@ -0,0 +1,15 @@
+def check(score): 
+  if score >=10 :
+    return "قبول شد"
+  else:
+      return "مردود شد"
+
+
+name= input (" ")
+age=int(input (" "))
+score=float(input (" "))
+
+print ("نام:" , name)
+print("سن:", age)
+print("نمره:" , score)
+print("نتیجه:" result)
