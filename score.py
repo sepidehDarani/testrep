c50f1b3819ef8846eba32018aa2b9a825694bdf0
sepidehDarani <darandashisepideh99@gmail.com>
2026-09-04 21:14:26 +0430

    [add]: add file

---
+++ score.py
@@ -0,0 +1,13 @@
+name=input("نام دانشجو")
+age=int(input("سن دانشجو"))
+score=float(input("نمره دانشجو"))
+
+if score>18 :
+    print ("الف")
+elif score13<نمره<18:
+    print("قابل قبول")
+elif score<10:
+    print("رد شده")
+else:
+    print("نیاز به بررسی")
+    
