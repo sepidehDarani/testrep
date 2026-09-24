3c06ec1b2c43a847e38aa189e6c003f7b198313f
sepidehDarani <darandashisepideh99@gmail.com>
2026-09-24 17:06:08 +0330

    [Add]: add file

--- mashinhesab 2.py
+++ mashinhesab 2.py
@@ -1,19 +1,3 @@
-def mashinhesab (num1 , num2 , amaliat):
-    
-
-    if amaliat == "+":
-       result=num1+num2
-    elif amaliat== "_":
-        result=num1-num2
-    elif amaliat=="*":
-       result=num1*num2
-    elif amaliat== "/":
-        result=num1/num2
-
-    return result 
-
-num1:float(input("عدد اول را وارد کنید"))
-num2:float(input("عدد دوم را وارد کنید"))
-amaliat:input("+ , - , * , / ")
-result= mashinhesab(num1 , num2 , amaliat)
-print(result)
+def sum_n(n1,n2):
+    res = n1+ n2
+    return res
